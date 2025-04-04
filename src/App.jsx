148c57778d7130bd6328e3c1984b71b23d478a8a
Tarık Taşkın <116636451+tarik-taskin@users.noreply.github.com>
2025-04-04 20:24:57 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Odevler from './Components/Odevler'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>
+        Merhaba Ahad.
+      </h1>
+      <Odevler/>
+    </div>
+  )
+}
+
+export default App
